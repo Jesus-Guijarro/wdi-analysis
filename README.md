ed0f7b1b02d5b5873b919b6ec284451e5a181217
Jesus-Guijarro <jguijarrosendra@gmail.com>
2024-12-12 12:06:49 +0100

    data analysis notebook

--- README.md
+++ README.md
@@ -13,23 +13,6 @@ wget https://databank.worldbank.org/data/download/WDI_CSV.zip
 unzip WDI_CSV.zip && rm WDI_CSV.zip
 ```
 
-### Project IntelliJ
-Java SDK version: 11
-
-```sbt
-ThisBuild / version := "0.1.0-SNAPSHOT"
-
-ThisBuild / scalaVersion := "2.13.10"
-
-lazy val root = (project in file("."))
-  .settings(
-    name := "wdi-analysis",
-    libraryDependencies ++= Seq(
-      "org.apache.spark" %% "spark-core" % "3.5.1",
-      "org.apache.spark" %% "spark-sql" % "3.5.1"
-    )
-  )
-```
 Check Spark version installed:
 ```shell
 spark-shell --version
--- get_WDI_data.sh
+++ get_WDI_data.sh
@@ -4,18 +4,12 @@ wget https://databank.worldbank.org/data/download/WDI_CSV.zip
 
 unzip WDI_CSV.zip 
 
-rm WDIcountry-series.csv
-rm WDIseries-time.csv
-rm WDIfootnote.csv
+rm WDIcountry-series.csv && rm WDIseries-time.csv && rm WDIfootnote.csv
 
-mv WDICSV.csv data/Countries_Indicators.csv
+mv WDICSV.csv data/Data.csv
 
-python3 clean_csv.py
+python3 transform_CSV.py
 
-mv Countries.csv data/Countries.csv
-mv Indicators.csv data/Indicators.csv
+mv Countries.csv data/Countries.csv && mv Indicators.csv data/Indicators.csv
 
-rm WDICountry.csv
-rm WDISeries.csv
-
-rm WDI_CSV.zip
+rm WDICountry.csv && rm WDISeries.csv && rm WDI_CSV.zip
